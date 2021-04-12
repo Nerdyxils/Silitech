ecfd5e5c2a261969d4b64cc7ba7c511d60044fdd
Nerdyxils <silasabiodun25@gmail.com>
2021-04-12 14:26:49 +0100

    images fix trial

--- src/components/pages/Services/Data.js
+++ src/components/pages/Services/Data.js
@@ -7,7 +7,7 @@ export const homeObjOne = {
     description : 'Get access to our exclusive diamond card that allows you to send unlimited transactions without getting charged fees',
     buttonLabel : 'Get Started',
     imgStart : '',
-    img: 'images/svg-1.svg',
+    img: './Silitech/images/svg-1.svg',
     alt : 'Credit Card'
 }
 
@@ -21,7 +21,7 @@ export const homeObjTwo = {
       'We have you covered no matter where you are located. Over 140 locations worldwide to ensure you have access anytime',
     buttonLabel: 'Learn More',
     imgStart: '',
-    img: 'images/svg-5.svg',
+    img: './Silitech/images/svg-5.svg',
     alt: 'Vault'
   };
   
@@ -35,7 +35,7 @@ export const homeObjTwo = {
       "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
     buttonLabel: 'Start Now',
     imgStart: 'start',
-    img: 'images/svg-7.svg',
+    img: './Silitech/images/svg-7.svg',
     alt: 'Vault'
   };
   
